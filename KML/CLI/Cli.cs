aea674dabc16e3e691b8c76516098ee6b5d0bcb1
my-th-os <mythos@fantasymail.de>
2021-02-23 16:59:45 +0100

    display info in CLI when no files given

--- KML/CLI/Cli.cs
+++ KML/CLI/Cli.cs
@@ -66,7 +66,7 @@ namespace KML
                 }
             }
 
-            if (Requested && filenames.Count == 0)
+            if (filenames.Count == 0)
             {
                 mode = Mode.Info;
             }
